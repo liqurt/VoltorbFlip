0f675a98d28d503b9214e2d74efabbf37bf53a2f
윤승일(Seung-Il Yoon) <liqurt@naver.com>
2020-02-20 17:07:35 +0900

    in GameActivity : bgm, effect sound is clear.

--- app/src/main/java/ysi/gachon/voltorbflip/GameActivity.kt
+++ app/src/main/java/ysi/gachon/voltorbflip/GameActivity.kt
@@ -2,6 +2,9 @@ package ysi.gachon.voltorbflip
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.media.SoundPool
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
@@ -9,18 +12,32 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_game.*
 import kotlin.random.Random
 
 
 class GameActivity : AppCompatActivity(), View.OnClickListener{
     companion object{
+        lateinit var bgmPlayer : MediaPlayer
         lateinit var pref : SharedPreferences
         lateinit var editor : SharedPreferences.Editor
+        var stage : Int = 0
         var highScore : Int = 0
         var previousScore : Int =0
         var currentScore : Int = 0
         val randomNumberPool : List<Int> = listOf(0,0,0,1,1,1,1,1,2,3)
+        val soundPool : SoundPool= SoundPool.Builder().build()
+        var soundWow : Int = 0
+        var soundYouDontHit : Int = 0
+        var soundBGM0 : Int = 0
+        var soundBGM1 : Int = 1
+        var soundBGM2 : Int = 2
+        var soundBGM3 : Int = 3
+        var soundBGM4 : Int = 4
+        var soundBGM5 : Int = 5
+        val soundBGMList : List<Int> = listOf(soundBGM0,soundBGM1,soundBGM2,soundBGM3,soundBGM4,soundBGM5)
     }
 
     var gameBoard = Array(5, {IntArray(5)})
@@ -29,11 +46,41 @@ class GameActivity : AppCompatActivity(), View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-
+        adviewSetup()
+        soundSetup()
         sharedPreferencesSetup()
         gameBoardSetUp()
         gameBoardSideSetUp()
         setOnClickListenerActivating()
+    }
+
+    private fun soundSetup() {
+        soundWow = soundPool.load(this, R.raw.wow, 2)
+        soundYouDontHit = soundPool.load(this, R.raw.youdonthit, 2)
+        soundBGM0 = soundPool.load(this, R.raw.pokemoncenter0, 1)
+        soundBGM1 = soundPool.load(this, R.raw.pokemoncenter1, 1)
+        soundBGM2 = soundPool.load(this, R.raw.pokemoncenter2, 1)
+        soundBGM3 = soundPool.load(this, R.raw.pokemoncenter3, 1)
+        soundBGM4 = soundPool.load(this, R.raw.pokemoncenter4, 1)
+        soundBGM5 = soundPool.load(this, R.raw.pokemoncenter5, 1)
+
+        val playThis = stage%6
+        when(playThis){
+            0->bgmPlayer = MediaPlayer.create(this, R.raw.pokemoncenter0)
+            1->bgmPlayer = MediaPlayer.create(this, R.raw.pokemoncenter1)
+            2->bgmPlayer = MediaPlayer.create(this, R.raw.pokemoncenter2)
+            3->bgmPlayer = MediaPlayer.create(this, R.raw.pokemoncenter3)
+            4->bgmPlayer = MediaPlayer.create(this, R.raw.pokemoncenter4)
+            5->bgmPlayer = MediaPlayer.create(this, R.raw.pokemoncenter5)
+        }
+        bgmPlayer.setLooping(true)
+        bgmPlayer.start()
+    }
+
+    private fun adviewSetup() {
+        MobileAds.initialize(this) {}
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 
     private fun sharedPreferencesSetup() {
@@ -232,9 +279,11 @@ class GameActivity : AppCompatActivity(), View.OnClickListener{
     }
 
     private fun gameOver(selectedView : ImageButton){
+        playYonDontHitSound()
         Toast.makeText(this,"Game over",Toast.LENGTH_SHORT).show()
         selectedView.setImageResource(R.drawable.cell0)
         setOnClickListenerDeactivating()
+        bgmPlayer.release()
         val handler = Handler()
         handler.postDelayed({
             initCurrentPreviousScore()
@@ -243,17 +292,28 @@ class GameActivity : AppCompatActivity(), View.OnClickListener{
     }
 
     private fun gameClear(){
+        playWowSound()
+        bgmPlayer.release()
         Log.d("tag","(clear)Current score : $currentScore")
         Toast.makeText(this,"You've got $currentScore in this game!",Toast.LENGTH_SHORT).show()
         setOnClickListenerDeactivating()
         previousScore += currentScore
         currentScore=0
         highScoreComparison()
+        stage++
         val handler = Handler()
         handler.postDelayed({
             finish()
             startActivity(getIntent())
-        }, 1000)
+        }, 1500)
+    }
+
+    private fun playYonDontHitSound(){
+        soundPool.play(soundYouDontHit, 1.0f, 1.0f,0,0,1.0f)
+    }
+
+    private fun playWowSound(){
+        soundPool.play(soundWow, 1.0f, 1.0f,0,0,1.0f)
     }
 
     private fun initCurrentPreviousScore(){
@@ -269,4 +329,6 @@ class GameActivity : AppCompatActivity(), View.OnClickListener{
             editor.commit()
         }
     }
+
+
 }
--- app/src/main/java/ysi/gachon/voltorbflip/MainActivity.kt
+++ app/src/main/java/ysi/gachon/voltorbflip/MainActivity.kt
@@ -1,9 +1,11 @@
 package ysi.gachon.voltorbflip
 
 import android.content.Intent
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        MobileAds.initialize(this) {}
+
         start.setOnClickListener{
             val gameIntent = Intent(this, GameActivity::class.java)
             startActivity(gameIntent)
         }
 
         quit.setOnClickListener{
-            Toast.makeText(this,"빠이",Toast.LENGTH_SHORT).show()
             finish()
         }
     }
